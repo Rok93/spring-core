3e258dd67f61e4730117aeb9047bc99cb574a586
rok93 <goodboy302@naver.com>
2021-04-26 00:59:10 +0900

    feat: 스프링으로 전환하기

--- src/main/java/com/example/springcore/AppConfig.java
+++ src/main/java/com/example/springcore/AppConfig.java
@@ -8,21 +8,28 @@ import com.example.springcore.domain.member.MemberServiceImpl;
 import com.example.springcore.domain.member.MemoryMemberRepository;
 import com.example.springcore.domain.order.OrderService;
 import com.example.springcore.domain.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
